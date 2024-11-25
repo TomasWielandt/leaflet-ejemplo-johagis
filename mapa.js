e0bfdf8d6e7f9600628e0b9ab432d633190d1e45
TomasWielandt <twielandt16@gmail.com>
2024-11-25 13:50:52 -0300

    ejemplo de leaflet

---
+++ mapa.js
@@ -0,0 +1,10 @@
+let map = L.map('map').setView([4.639386, -74.082412], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+document.getElementById('select-location').addEventListener('change', function(e){
+    let coords = e.target.value.split(",");
+    map.flyTo(coords, 13);
+});
